92a28d8f0b79a93c81348af3d0c11294d4b586c6
Roman Dodenko <creed6695@mail.ru>
2023-05-18 21:38:19 +0700

    a

--- js/main.js
+++ js/main.js
@@ -5,12 +5,12 @@ const gif = document.querySelector(".hero__video");
 if (gif) {
   if (document.body.clientWidth >= 1024) {
     let template = `
-    <img src="./img/decstop.gif" loading="lazy" width="300" height="300" alt="Gif">
+    <img src="./img/decstop.gif" loading="lazy" width="300" height="300" alt="Wolf treasure">
     `;
     gif.insertAdjacentHTML("beforeend", template);
   }  else if (document.body.clientWidth <= 992) {
     let template = `
-    <img src="./img/tablet.gif" loading="lazy" width="300" height="300" alt="Gif">
+    <img src="./img/tablet.gif" loading="lazy" width="300" height="300" alt="Wolf treasure">
     `;
     gif.insertAdjacentHTML("beforeend", template);
   }
